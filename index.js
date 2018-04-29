5cff4260e4af4d36e69783a0f7340c8fad6aff8a
Blue PandaPL <38743467+bluepandapl@users.noreply.github.com>
2018-04-28 20:16:42 -0700

    Add issue commenter

--- index.js
+++ index.js
@@ -7,4 +7,17 @@ module.exports = (robot) => {
 
   // To get your app running against GitHub, see:
   // https://probot.github.io/docs/development/
+
+  robot.on('issues.opened', async context => {
+    robot.log('Got opened issue!')
+
+
+    // `context` extracts information from the event, which can be passed to
+    // GitHub API calls. This will return:
+    //   {owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World!}
+    const params = context.issue({body: 'Hello World!'})
+
+    // Post a comment on the issue
+    return context.github.issues.createComment(params)
+  })
 }
